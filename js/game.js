afe11dfef337df93ce386a9417437081a3297cf7
Andrea Pérez <andreaa.perz@gmail.com>
2021-11-24 15:18:11 -0600

    git revert on Felix

--- js/game.js
+++
@@ -1,6 +0,0 @@
-var span = document.getElementsByClassName("close")[0];
-var modal = document.getElementById("vidtop-content");
-
-span.onclick = function () {
-    modal.style.display = "none";
-};
